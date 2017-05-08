c683a38acf2b13d888aac65517f40a0685476de4
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-05-07 17:54:35 -0700

    add one more map test

--- test/map.test.js
+++ test/map.test.js
@@ -7,6 +7,29 @@ describe('map()', () => {
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
 
+  it('maps every value in an object to their square', () => {
+    const obj = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4
+    };
+
+    let mappedObj = _.map(obj, (element) => element * element);
+    expect(mappedObj).toEqual([1,4,9,16]);
+  })
+
+  it('maps every value in an object', () => {
+    const objTwo = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4
+    };
+    let mappedObjTwo = _.map(objTwo, (element, key, obj) => obj[key]);
+    expect(mappedObjTwo).toEqual([1,2,3,4]);
+  })
+
   it('maps every number in an array to their index', () => {
   	let arr = [1, 2, 3, 4, 5];
   	let mappedArr = _.map(arr, (element,index) => index);
